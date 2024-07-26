a619ccfd58fd0c71601920954ad25623e1483705
ManPatel-07 <patelman7720@gmail.com>
2024-07-26 07:57:16 +0530

    Employee Details Save RestAPI
    Employee Details Save RestAPI in Spring Boot  also use Mysql DataBase

--- SpringBoot-employee-details/src/main/java/org/mm/dto/EmployeeDto.java
+++ SpringBoot-employee-details/src/main/java/org/mm/dto/EmployeeDto.java
@@ -12,4 +12,57 @@ public class EmployeeDto
 	private String email;
 
 	private String department;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public EmployeeDto(Long id, String name, String email, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.department = department;
+	}
+
+	public EmployeeDto(String name, String email, String department) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.department = department;
+	}
+
+	public EmployeeDto() {
+		super();
+	}
+	
+	
 }
